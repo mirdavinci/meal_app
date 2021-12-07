f72047b6dc03e786c4145ac3d4ac2e67654dff92
mirdavinci <mirdarmohsen1@gmail.com>
2021-12-07 20:43:40 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var phoneHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
